d23b0aecd29b029308b41f2f1152c4ce99e2de24
Kelton <keltonkuan@gmail.com>
2025-04-22 14:25:31 -0400

    Changed hero movement from mouseclick to key down

--- Hero.java
+++ Hero.java
@@ -2,18 +2,13 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class Hero extends Actor
 {
-    boolean atTop = true;
-    
     public void act()
     {
-        if (Greenfoot.mouseClicked(null)) {
-            atTop = !atTop;
+        if (Greenfoot.isKeyDown("w")) {
+            setLocation(getX(), getY() - 5);
         }
-        
-        if (atTop) {
-            setLocation(100, 100);
-        } else {
-            setLocation(100, 300);
+        if (Greenfoot.isKeyDown("s")) {
+            setLocation(getX(), getY() + 5);
         }
     }
 }
