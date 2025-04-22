2d087d7921eb5bb2cf203132e16f4d3c04af43ed
Kelton <keltonkuan@gmail.com>
2025-04-22 14:28:20 -0400

    Added start

--- MyWorld.java
+++ MyWorld.java
@@ -3,7 +3,10 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
-        
+        start();
+    }
+    
+    public void start() {
         Hero hero = new Hero();
         addObject(hero, 100, 100);
         
