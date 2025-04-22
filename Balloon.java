2d1a9c8cb7e8563adfcde2d4fea272751b92cd21
Joe <53659792+joez-zh@users.noreply.github.com>
2025-04-22 14:13:31 -0400

    added balloon and sad face

---
+++ Balloon.java
@@ -0,0 +1,27 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Balloon here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Balloon extends Actor
+{
+    /**
+     * Act - do whatever the Balloon wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() {
+        move(-10);
+            
+        if (getX() <= 0) {
+            resetBalloon();
+        }
+    }
+
+    public void resetBalloon() {
+        int num = Greenfoot.getRandomNumber(8);
+        setLocation(600, (num+1)*50);
+    }
+}
