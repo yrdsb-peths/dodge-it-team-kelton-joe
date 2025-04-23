5b5ba653b8b8cbc11338073444036521a192259d
Joe <53659792+joez-zh@users.noreply.github.com>
2025-04-23 13:55:37 -0400

    added sadface image

--- SadFace.java
+++ SadFace.java
@@ -1,19 +1,13 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class SadFace here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class SadFace extends Actor
 {
-    /**
-     * Act - do whatever the SadFace wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    public SadFace() {
+        setImage("images/smiley3.png");
+    }
+    
     public void act()
     {
-        // Add your action code here.
+        
     }
 }
